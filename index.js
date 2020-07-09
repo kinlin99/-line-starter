f1d266477f3e349530b53c5bc7dff167b56873e4
kinlin99 <67885326+kinlin99@users.noreply.github.com>
2020-07-09 11:54:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 var request = require('request');
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
+var userId='';
+var displayName='';
+var serial='';
 
 app.use(express.static('public'));
 
@@ -12,26 +15,33 @@ app.get('/send-id', function(req, res) {
 });
 
 app.get('/device', function(req, res) {
-    console.log(port);
+    console.log(funcEP+'/device')
     console.log("Serial : "+req.query.serial);
-    request.get("https://line-starter-v2.herokuapp.com/test", function (err, resp, body) {
-        if (err) {
-            console.log('Error!');
-        } else {
-            console.log('OK!');
-        }
-    });
+    serial = req.query.serial;
     res.end();
 });
 
 app.get('/profile', function(req, res) {
-    console.log(req.query.userId);
-    console.log(req.query.displayName);
+    console.log(funcEP+'/profile')
+    console.log("userId : "+req.query.userId);
+    console.log("displayName : "+req.query.displayName);
+    userId = req.query.userId;
+    displayName = req.query.displayName;
     res.end();
+    var reqUrl = "https://line-starter-v2.herokuapp.com/devicecb?"+"serial="+serial+"&"+"userId="+userId;
+    request.get(, function (err, resp, body) {
+        if (err) {
+            console.log(funcEP+'Error!');
+        } else {
+            console.log(funcEP+'OK!');
+        }
+    });
 });
 
-app.get('/test', function(req, res) {
-    console.log('/test OK!');
+app.get('/devicecb', function(req, res) {
+    console.log(funcEP+'/devicecb')
+    console.log('serial : '+req.query.serial);
+    console.log('userId : '+req.query.userId);
     res.end();
 });
 
